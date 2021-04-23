60f375061da42c897e6210bf3abacaae985032f8
Tom Cumbow <tcumbow@icloud.com>
2021-04-23 09:53:01 -0400

    auto-formatting

--- Azure Backup Policy Tags.ps1
+++ Azure Backup Policy Tags.ps1
@@ -3,8 +3,8 @@
 # Required module: Az.RecoveryServices
 
 param(
-    [parameter(Mandatory=$false)]
-    [bool]$SimulationOnly = $false
+	[parameter(Mandatory=$false)]
+	[bool]$SimulationOnly = $false
 )
 
 function Log ($Text) {
@@ -12,62 +12,54 @@ function Log ($Text) {
 }
 
 # Define backup policies by tag and region
-function DetermineBackupPolicy ([string]$tag, $region)
-{
-	$processedTag = $tag.replace("BackupRP","Backup")
+function DetermineBackupPolicy ([string]$tag, $region) {
+	$processedTag = $tag.replace("BackupRP", "Backup")
 	return "$region-$processedTag"
 }
 
 # Main runbook content
-try
-{
+try {
 	$ScriptStartTime = (Get-Date).ToUniversalTime()
-    Log "Runbook started."
-    if($Simulate)
-    {
-        Log "*** Running in SIMULATE mode. No actions will be taken. ***"
-    }
-    else
-    {
-        Log "*** Running in LIVE mode. Policies will be enforced. ***"
-    }
+	Log "Runbook started."
+	if ($Simulate) {
+		Log "*** Running in SIMULATE mode. No actions will be taken. ***"
+	}
+	else {
+		Log "*** Running in LIVE mode. Policies will be enforced. ***"
+	}
 
-    # Authentication and connection
-    if (-not $LocalDevMode) {
-        $connectionName = "AzureRunAsConnection"
-        $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName
-        $DummyVariable = $(Add-AzAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint)
-        Log "Successfully logged into Azure subscription using Az cmdlets..."
-    }
+	# Authentication and connection
+	if (-not $LocalDevMode) {
+		$connectionName = "AzureRunAsConnection"
+		$servicePrincipalConnection=Get-AutomationConnection -Name $connectionName
+		$DummyVariable = $(Add-AzAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint)
+		Log "Successfully logged into Azure subscription using Az cmdlets..."
+	}
 
-    Log "Getting all the resources from the subscription..."
-    $AllResources = Get-AzResource
+	Log "Getting all the resources from the subscription..."
+	$AllResources = Get-AzResource
 
-    Log "Processing [$($AllResources.Count)] resources found in subscription"
-    foreach($resource in $AllResources)
-    {
-        # Check for tag
-		if($resource.ResourceType -eq "Microsoft.Compute/virtualMachines" -and $resource.Tags.BackupPolicy)
-		{
+	Log "Processing [$($AllResources.Count)] resources found in subscription"
+	foreach ($resource in $AllResources) {
+		# Check for tag
+		if ($resource.ResourceType -eq "Microsoft.Compute/virtualMachines" -and $resource.Tags.BackupPolicy) {
 			$PolicyText = $resource.Tags.BackupPolicy
 			Log "[$($resource.Name)]: Found BackupPolicy tag with value: $PolicyText"
 		}
-        else
-        {
-            Log "[$($Resource.Name)]: Not tagged for backup policy. Skipping this resource."
-            continue
-        }
+		else {
+			Log "[$($Resource.Name)]: Not tagged for backup policy. Skipping this resource."
+			continue
+		}
 
-        # Check that tag value was successfully obtained
-        if($null -eq $PolicyText)
-        {
-            Log -Warning "[$($Resource.Name)]: Failed to get tag, skipping this resource."
-            continue
-        }
+		# Check that tag value was successfully obtained
+		if ($null -eq $PolicyText) {
+			Log -Warning "[$($Resource.Name)]: Failed to get tag, skipping this resource."
+			continue
+		}
 
 		# Enact backup policy based on tag
 		if ($PolicyText -eq "SpecialPolicy") {
-			$VaultID = Get-AzRecoveryServicesVault -ResourceGroupName "Sandbox" -Name "Test-Vault" | select -ExpandProperty ID
+			$VaultID = Get-AzRecoveryServicesVault -ResourceGroupName "Sandbox" -Name "Test-Vault" | Select-Object -ExpandProperty ID
 			Log "VaultID $VaultID"
 			$PolicyObject = Get-AzRecoveryServicesBackupProtectionPolicy -Name "SpecialPolicy" -VaultId $VaultID
 			$PolicyObject | ConvertTo-Json | Log
@@ -76,17 +68,15 @@ try
 			Enable-AzRecoveryServicesBackupProtection -Item $ExistingBackupItem -Policy $PolicyObject -VaultId $VaultID
 			# Enable-AzRecoveryServicesBackupProtection -Policy $PolicyObject -Name $resource.Name -ResourceGroupName $resource.ResourceGroupName -VaultId $VaultID
 		}
-    }
+	}
 
-    Log "Finished processing Azure resources"
+	Log "Finished processing Azure resources"
 }
-catch
-{
-    $errorMessage = $_.Exception.Message
+catch {
+	$errorMessage = $_.Exception.Message
 	Log -Error "SEVERE Unexpected exception: $errorMessage"
-    throw "Unexpected exception: $errorMessage"
+	throw "Unexpected exception: $errorMessage"
 }
-finally
-{
-    Log "Runbook finished (Duration: $(("{0:hh\:mm\:ss}" -f ((Get-Date).ToUniversalTime() - $ScriptStartTime))))"
+finally {
+	Log "Runbook finished (Duration: $(("{0:hh\:mm\:ss}" -f ((Get-Date).ToUniversalTime() - $ScriptStartTime))))"
 }
